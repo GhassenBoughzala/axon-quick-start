3129002d6f5f4c7aba4150193fd06f135e727266
GhassenBoughzala <ghassen.boughzala@esprit.tn>
2023-01-11 14:21:03 +0100

    Set Room Aggregate Class

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,6 +1,30 @@
 package io.axoniq.labs.chat.commandmodel;
 
+import io.axoniq.labs.chat.coreapi.CreateRoomCommand;
+import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
 public class ChatRoom {
 
-    // TODO: This class has just been created to make the test compile. It's missing, well, everything...
+    @AggregateIdentifier
+    private String roomId;
+    private boolean roomConfirmed;
+
+    @CommandHandler
+    public ChatRoom(CreateRoomCommand command){
+        AggregateLifecycle.apply(new RoomCreatedEvent(command.getRoomId(), command.getName() ));
+    }
+
+    @EventSourcingHandler
+    public void on(RoomCreatedEvent event){
+        this.roomId = event.getRoomId();
+        roomConfirmed = false;
+    }
+
+    protected ChatRoom(){}
 }
